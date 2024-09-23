bad54a6bbee2dd2242786f4ee735539f3c0f7600
Pryk7642 <phiriyakorn.p@kkumail.com>
2024-09-24 02:07:16 +0700

    ff

---
+++ src/components/todos/ChangeTodo.tsx
@@ -0,0 +1,22 @@
+import { todoProps } from "@/types";
+import Button from "../button/Button";
+import Form from "../form/Form";
+import Input from "../input/Input";
+import * as actions from "@/actions";
+import { FaCheck } from "react-icons/fa";
+
+const ChangeTodo = ({ todo }: { todo: todoProps }) => {
+    return (
+      <form action={actions.changeStatus}>
+        <Input name="inputId" value={todo.id} type="hidden"></Input>
+        <Button
+          text={<FaCheck />}
+          type="submit"
+          actionButton
+          bgColor={todo.isCompleted ? "bg-green-400" : "bg-blue-500"} // ใช้ isCompleted
+        ></Button>
+      </form>
+    );
+  };
+
+export default ChangeTodo;
